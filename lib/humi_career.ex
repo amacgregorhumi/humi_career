3662dd3457eb3294b70aa8a63110d879ad710661
Allan MacGregor <allan@humi.ca>
2022-05-05 10:35:15 -0400

    Rough working version

--- lib/humi_career.ex
+++ lib/humi_career.ex
@@ -4,12 +4,11 @@ defmodule HumiCareer do
   @lever_endpoint  "https://api.lever.co/v1/"
   @webflow_endpoint  "https://api.webflow.com/"
   @web_collection_id  "6234a69edb0b4b06e73d515f"
+  @team_collection_id "62334182fb264f451a431626"
 
   def main() do
     lever_postings = retrieve_postings("lever")
     web_flow_postings = retrieve_postings("webflow")
-
-    IO.inspect(web_flow_postings)
 
     #Webflow IDS
     webflow_ids =
@@ -68,13 +67,16 @@ defmodule HumiCareer do
 
     ## Find the webflow id from the list
     web_flow_postings = retrieve_postings("webflow")
-
     webflow_id = Enum.find(web_flow_postings["items"], fn (wfp) -> wfp["lever-uuid"] == posting[:lever_uuid] end)
 
     credentials = "f05f256dd66a6f010e3b025287dd9fe67fc8c6a7a9c30c37b39f5cb05c272ac7"
     headers = ["Authorization": "Bearer #{credentials}", "Content-Type": "application/json", "accept-version": "1.0.0"]
     url = @webflow_endpoint <> "collections/" <> @web_collection_id <> "/items/" <> webflow_id["_id"]
 
+    teams = retrieve_teams()
+    team = Enum.find(teams["items"], fn (t) -> t["name"] == posting[:team] end)
+
+    IO.inspect(teams)
 
     body = Poison.encode!(
       %{ "fields" => %{
@@ -83,7 +85,8 @@ defmodule HumiCareer do
         "name" => posting[:title],
         "lever-uuid" => posting[:lever_uuid],
         "slug" => posting[:slug],
-        "team" => posting[:team],
+        "team" => team["name"],
+        "team-reference" => team["_id"],
         "commitment" => posting[:commitment],
         "location" => posting[:location],
         "description-html" => posting[:description_html],
@@ -92,11 +95,13 @@ defmodule HumiCareer do
       }}
     )
 
-    {:ok, response} = HTTPoison.post(url, body, headers, [])
+    {:ok, response} = HTTPoison.put(url, body, headers, [])
+    IO.inspect(response)
+
+
     {:ok, results} = response.body() |> JSON.decode()
 
     results
-
   end
 
   def create_item (posting) do
@@ -107,6 +112,12 @@ defmodule HumiCareer do
     headers = ["Authorization": "Bearer #{credentials}", "Content-Type": "application/json", "accept-version": "1.0.0"]
     url = @webflow_endpoint <> "collections/" <> @web_collection_id <> "/items"
 
+    teams = retrieve_teams()
+    team = Enum.find(teams["items"], fn (t) -> t["name"] == posting[:team] end)
+
+
+
+
     body = Poison.encode!(
       %{ "fields" => %{
         "_archived" => false,
@@ -114,7 +125,8 @@ defmodule HumiCareer do
         "name" => posting[:title],
         "lever-uuid" => posting[:lever_uuid],
         "slug" => posting[:slug],
-        "team" => posting[:team],
+        "team" => team["name"],
+        "team-reference" => team["_id"],
         "commitment" => posting[:commitment],
         "location" => posting[:location],
         "description-html" => posting[:description_html],
@@ -141,6 +153,17 @@ defmodule HumiCareer do
     end
   end
 
+  def retrieve_teams() do
+    credentials = "f05f256dd66a6f010e3b025287dd9fe67fc8c6a7a9c30c37b39f5cb05c272ac7"
+    headers = ["Authorization": "Bearer #{credentials}", "Accept": "application/json", "accept-version": "1.0.0"]
+    url = @webflow_endpoint <> "collections/" <> @team_collection_id <> "/items"
+
+    {:ok, response} = HTTPoison.get(url,headers, [])
+    {:ok, results} = response.body() |> JSON.decode()
+
+    results
+  end
+
   def retrieve_postings(source = "webflow") do
     credentials = "f05f256dd66a6f010e3b025287dd9fe67fc8c6a7a9c30c37b39f5cb05c272ac7"
     headers = ["Authorization": "Bearer #{credentials}", "Accept": "application/json", "accept-version": "1.0.0"]
@@ -159,7 +182,6 @@ defmodule HumiCareer do
     url = @lever_endpoint <> "postings"
 
     {:ok, response} = HTTPoison.get(url,headers, [])
-
     {:ok, results} = response.body() |> JSON.decode()
 
     postings = results["data"] |> Enum.map(&parse_posting/1)
